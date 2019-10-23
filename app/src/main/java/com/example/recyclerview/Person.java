8bfe45e014478f4389cf5ceea57d63f01958e7d3
patrickfeltes <pj6444@gmail.com>
2019-10-23 14:56:53 -0500

    Update with insert, delete, and get from roomdatabase

--- app/src/main/java/com/example/recyclerview/Person.java
+++ app/src/main/java/com/example/recyclerview/Person.java
@@ -1,10 +1,21 @@
 package com.example.recyclerview;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "person")
 public class Person {
 
-    private String name;
-    private int age;
+    @PrimaryKey(autoGenerate = true) private long id;
+    @ColumnInfo(name = "name") private String name;
+    @ColumnInfo(name = "age") private int age;
 
+    // empty constructor for room
+    public Person() { }
+
+    @Ignore
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
@@ -16,5 +27,21 @@ public class Person {
 
     public int getAge() {
         return age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
